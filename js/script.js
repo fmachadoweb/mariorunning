f452c25cf125908b432908f4ae168cd714ce17ce
Fernando Machado <fernandomachadoweb@gmail.com>
2022-07-20 11:31:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/* Created By Fernando Machado
+    My github: https://github.com/fmachadoweb
+    */
+
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout (() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 200) {
+
+        pipe.style.animation ='none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation ='none';
+        mario.style.bottom = `${pipePosition}px`;
+
+        mario.src ='./img/game-over.png';
+        mario.style.width = "120px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
+
+
